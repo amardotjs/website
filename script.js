fceae47699198b2ae7053dc849401fdcf9c6220a
Amar Ganesh Pandian <109662654+amardotjs@users.noreply.github.com>
2024-04-17 02:16:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function instagram(){
+	window.open("https://www.instagram.com/immortal.py/")
+}
+function discord(){
+	alert("infog007 is my discord ID");
+}
+function github(){
+	window.open("https://github.com/amardotjs");
+}
